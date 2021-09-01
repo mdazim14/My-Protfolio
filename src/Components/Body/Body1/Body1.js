1441bb6df4b0a6010188e42500e5b32ad5861c67
Md Azim <mdazim.cse@gmail.com>
2021-09-02 00:36:04 +0600

    added style

--- src/Components/Body/Body1/Body1.js
+++ src/Components/Body/Body1/Body1.js
@@ -3,14 +3,14 @@ import azimP1 from '../../../images/azimP1.png';
 
 const Body1 = () => {
     return (
-        <div className="p-5 d-flex bg-info justify-content-center">
-            <div className="col-6 border-bottom border-warning p-5">
+        <div className="p-5 d-flex bg-dark text-white justify-content-center">
+            <div className="col-6 p-5 border-top border-bottom border-warning text-left">
                 <h1>Hi! I'm</h1>
-                <h1>Md Azim Howlader</h1>
-                <h5>JavaScript Developer</h5>
+                <h1 className="text-warning">Md Azim Howlader</h1>
+                <h4>JavaScript Developer</h4>
                 <p>A self-motivated and enthusiastic web developer. To gain confidence and fame using my potential in the field of web development and express my innovative creative skills for self and company growth. I always like to learn new things and have experience with new stuff.</p>
             </div>
-            <div className="col-6 border-top border-warning">
+            <div className="col-6 p-5 border-bottom border-top border-warning">
                 <img className="img-fluid" src={azimP1} alt="" />
             </div>
         </div>

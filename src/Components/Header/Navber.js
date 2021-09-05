7a1adc3e7d143beec888956b755faab81ae8e3f0
Md Azim <mdazim.cse@gmail.com>
2021-09-06 03:15:19 +0600

    style of navber

--- src/Components/Header/Navber.js
+++ src/Components/Header/Navber.js
@@ -14,14 +14,14 @@ const Navber = () => {
             <Navbar className="fs-5 fw-bold text-white" collapseOnSelect expand="lg" bg="dark">
                 <Container>
                     <Navbar.Brand className="fs-3 text-warning border-bottom border-warning" href="/">[ MD AZIM ]</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav " />
-                    <Navbar.Collapse id="responsive-navbar-nav ">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto fw-bold">
-                            <a className=" m-2 border-bottom border-warning p-2" href="/">Home</a>
-                            <a className=" m-2 border-bottom border-warning p-2" href="/about">About</a>
-                            <a className=" m-2 border-bottom border-warning p-2" href="/project">Project</a>
-                            <a className=" m-2 border-bottom border-warning p-2" href="/blog">Blog</a>
-                            <a className=" m-2 border-bottom border-warning p-2" href="/contact">Contact</a>
+                            <a className=" m-2  link-light p-2" href="/">Home</a>
+                            <a className=" m-2  link-light p-2" href="/about">About</a>
+                            <a className=" m-2  link-light p-2" href="/project">Project</a>
+                            <a className=" m-2  link-light p-2" href="/blog">Blog</a>
+                            <a className=" m-2  link-light p-2" href="/contact">Contact</a>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
